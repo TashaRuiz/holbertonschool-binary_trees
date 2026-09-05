a38f2b338e4a2c2633f1b363ef10ab21ec7b2516
TashaRuiz <ruiztasha25@gmail.com>
2026-09-05 09:16:45 -0400

    Write a function that inserts a node as the right-child of another node
    Prototype: binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
    Where parent is a pointer to the node to insert the right-child in
    And value is the value to store in the new node
    Your function must return a pointer to the created node, or NULL on failure or if parent is NULL
    If parent already has a right-child, the new node must take its place, and the old right-child must be set as the right-child of the new node.

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,7 +7,7 @@
  * Return: Pointer to the created node, or NULL on failure 
  */ 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value) 
-{ 
+{
 	binary_tree_t *new_node; 
 	if (parent == NULL)
 	{	
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 	{
 		return (NULL);
-	}	
+	}
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL; 
@@ -27,7 +27,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		parent->right->parent = new_node;
-	}	
+	}
 	parent->right = new_node; 
 
 	return (new_node);
